6b2246d6f707c9ce4d4164f0e725da0351c0749c
VeryverySoHappy <frontEnd0815@gmail.com>
2022-07-11 18:19:23 +0900

    JSX에서 자바스크립트 사용하기

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import ReactDOM from "react-dom";
 
+const product = 'Macbook';
+const model = ' Air';
+const item = product + model;
+const imageUrl = 'https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRZgv8Gg0gc22EmVVHQJ169M8Y4l1-rBZB9HlL_dif2MbSKf_tYAIsFCh1FQwHS71ok45IrJ6JZVw&usqp=CAc'
+
+function handleClick(){
+  alert('곧 도착합니다');
+}
 ReactDOM.render(
   <>
-    <h1>가위, 바위, 보</h1>
-    <button className="hand">가위</button>
-    <button className="hand">바위</button>
-    <button className="hand">보</button>
+    <h1>나만의 {item} 주문하기</h1>
+    <img src={imageUrl} alt='제품사진'/>
+    <button onClick={handleClick}>확인</button>
   </>,
   document.getElementById('root')
 );
